6416e5cc0a1e1019874cea99d7cc035ac22c9939
Diky-Without-C <snayzher@gmail.com>
2024-06-08 18:24:25 +0700

    fix: time issue

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,11 +2,11 @@ export default function getCurrentTime(lang) {
   const date = new Date();
   const hour = date.getHours();
 
-  if (hour >= 4) {
+  if (hour >= 4 && hour < 10) {
     return lang == "id" ? "pagi" : "morning";
-  } else if (hour >= 10) {
+  } else if (hour >= 10 && hour < 15) {
     return lang == "id" ? "siang" : "noon";
-  } else if (hour >= 15) {
+  } else if (hour >= 15 && hour < 18) {
     return lang == "id" ? "sore" : "afternoon";
   } else {
     return lang == "id" ? "malam" : "evening";
